c370ad851c37d8c17f0075edc014a20f76f2a839
Shengyou Fan <shengyoufan@gmail.com>
2016-02-10 23:34:20 +0800

    [單元六 - CRUD 與 Validation] 練習 2：設定頁面邏輯及表單動作

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -21,7 +21,7 @@
 				</div>
 
 				<!-- 建立新 Task 表單 -->
-				<form action="" method="POST" class="form-horizontal">
+				<form action="{{ url('tasks') }}" method="POST" class="form-horizontal">
 					{{ csrf_field() }}
 
 					<!-- Task 名稱 -->
@@ -61,20 +61,22 @@
 					@foreach($tasks as $task)
 						<tr>
 							<td class="table-text">
-								<div class="">{{ $task->name }}</div>
+								<div class="{{ ($task->done == 1)? ' check' : '' }}">{{ $task->name }}</div>
 							</td>
 							<td>
+								@if ($task->done == 0)
 								<!-- 完成 Task 按鈕 -->
-								<form action="" method="POST" class="form-inline">
+								<form action="{{ url('tasks/'.$task->id) }}" method="POST" class="form-inline">
 									{{ csrf_field() }}
     								{{ method_field('PATCH') }}
-    								
+
 									<button type="submit" class="btn btn-success">
 										<i class="fa fa-check"></i>完成
 									</button>
 								</form>
+								@endif
 								<!-- 刪除 Task 按鈕 -->
-								<form action="" method="POST" class="form-inline">
+								<form action="{{ url('tasks/'.$task->id) }}" method="POST" class="form-inline">
 									{{ csrf_field() }}
     								{{ method_field('DELETE') }}
 
