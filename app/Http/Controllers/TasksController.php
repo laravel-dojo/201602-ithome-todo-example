0357642ff1704c801aec09bb8b8d97cba8e67cfe
Shengyou Fan <shengyoufan@gmail.com>
2016-02-10 23:49:08 +0800

    [單元六 - CRUD 與 Validation] 練習 7：增加表單驗證並顯示錯誤訊息

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -20,6 +20,10 @@ class TasksController extends Controller
 
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'name' => 'required'
+        ]);
+        
         Task::create($request->all());
 
     	return redirect('/');
